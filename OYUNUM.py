d728a7e32176ae5f8c52cf8268c1ff7205fdffef
RESADSTEAM3 <127946136+RESADSTEAM3@users.noreply.github.com>
2023-03-15 15:03:38 +0400

    Python Sayı oyunu
    Bu bir sayı oyunudur. Pythonda Yapılmısdır.

---
+++ OYUNUM.py
@@ -0,0 +1,12 @@
+import random
+j = random.randint(1,5)
+print("Sayı tuttum, texmin et!")
+
+while True:
+    n = int(input("Texminivizi yazın: "))
+    if n == j:
+        print("Duzdu Ne ecebse")
+        break
+    else:
+        print("Uduzdun hahahahah")
+        break
